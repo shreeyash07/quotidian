f720f5858d24feaa6659b4e0727339d02d48fd3d
shreeyash07 <shresthashree0807@gmail.com>
2020-08-06 20:01:08 +0545

    expenses push

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:quotidian/widgets/transaction.dart';
+
+void main() => runApp(Quotidian());
 
 class Quotidian extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.purple,
+        errorColor: Colors.red,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(fontFamily: 'OpenSans', fontSize: 20),
+              button: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
   }
 }
