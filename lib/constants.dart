4e35901c6ea9475b5908f28dfe57afbea83281d3
shreeyash07 <shresthashree0807@gmail.com>
2020-08-15 10:24:36 +0545

    splashscreen added

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const qPrimaryColor = Color(0xFFFF7643);
+const qPrimaryLightColor = Color(0xFFFFECDF);
+const qPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const qSecondaryColor = Color(0xFF979797);
+const qTextColor = Color(0xFF757575);
+
+const qAnimationDuration = Duration(milliseconds: 200);
